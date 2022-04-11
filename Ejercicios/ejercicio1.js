4637e9634e56d5670dea02cd5c482f59a6fa744c
FacuGut <101258757+FacuGut@users.noreply.github.com>
2022-04-11 08:27:11 -0300

    Add files via upload

---
+++ Ejercicios/ejercicio1.js
@@ -0,0 +1,11 @@
+var arr = [11,33,2,-1,110,99,8];
+PMI=false
+i=0
+arr.sort(function(a,b){return a - b;}).reverse();
+while(!PMI || arr[i]%2==0){
+    if(arr[i]%2!=0){
+        PMI=true
+    }
+    i++;
+}
+console.log(arr[i])
